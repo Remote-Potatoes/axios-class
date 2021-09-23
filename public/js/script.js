10d53bd11f1e79bf10d97eb35a27acf7c731bd73
Filipe <filipefreire@protonmail.com>
2021-09-23 18:08:24 +0200

    add: axios API call on both Backend & Frontend

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,33 @@ document.addEventListener(
   },
   false
 );
+
+const avatar = document.querySelector(".card--avatar");
+const charName = document.getElementById("char-name");
+console.log("name:", charName);
+const charGender = document.getElementById("char-gender");
+console.log("gender:", charGender);
+const charStatus = document.getElementById("char-status");
+console.log("status:", charStatus);
+
+const btn = document.getElementById("getAnotherChar");
+btn.addEventListener("click", () => {
+  axios
+    .get(
+      `https://rickandmortyapi.com/api/character/${Math.floor(
+        Math.random() * 671
+      )}`
+    )
+    .then((response) => {
+      console.log("🔥 HERE IS OUR RESPONSE FROM AXIOS:", response.data);
+      const { image, name, status, gender } = response.data;
+
+      avatar.src = image;
+      charName.innerText = name;
+      charStatus.innerText = status;
+      charGender.innerText = gender;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
+const { default: axios } = require("axios");
+
 const router = require("express").Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  axios
+    .get(
+      `https://rickandmortyapi.com/api/character/${Math.floor(
+        Math.random() * 671
+      )}`
+    )
+    .then((response) => {
+      console.log("🔥 HERE IS OUR RESPONSE FROM AXIOS:", response.data);
+      const { image, name, status, gender } = response.data;
+      res.render("index", {
+        character: {
+          avatar: image,
+          name,
+          gender,
+          status,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("index", {
+        character: {
+          avatar: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+          name: "Healhty Morty",
+          gender: "male",
+          status: "dead",
+        },
+      });
+    });
 });
 
 module.exports = router;
